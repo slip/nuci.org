f44b98060f32ca727db1e1a084b77e82de336dcd
Ian Kennedy <exposure@gmail.com>
2016-07-22 10:33:50 -0400

    Added contact and social icons

--- wp-content/themes/nuci2016/footer.php
+++ wp-content/themes/nuci2016/footer.php
@@ -34,12 +34,12 @@
           <li>Rock'N'Raffle</li>
           <li>The Steeple</li>
         </ul>
-      </div>      
+      </div>
       <div class="footer-nav">
         <h4 class="footer-nav-header">Programs</h4>
         <ul>
           <li>Camp Amped</li>
-          <li>Health & Wellness</li>
+          <li>Health &amp; Wellness</li>
           <li>Musician's Services</li>
         </ul>
       </div>
@@ -51,7 +51,27 @@
           <li>Auction</li>
         </ul>
       </div>
+      <div class="footer-nav">
+        <h4 class="footer-nav-header">Quick Contact</h4>
+        <ul>
+          <li><i class="icon-email"></i>space@nuci.org</li>
+          <li><i class="icon-phone"></i>706.227.1515</li>
+          <li><i class="icon-fax"></i>706.227.1524</li>
+          <li><i class="icon-address"></i>396 Oconee Street, Athens GA</li>
+        </ul>
+      </div>
 		</div><!-- .site-info -->
+    <div class="site-info">
+      <div class="follow-us">
+        <h4 class="footer-nav-header">Follow Us</h4>
+        <ul>
+          <li class="icon-facebook">Facebook</li>
+          <li class="icon-twitter">Twitter</li>
+          <li class="icon-flickr">Flickr</li>
+          <li class="icon-google-plus">Google+</li>
+        </ul>
+      </div>
+    </div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
