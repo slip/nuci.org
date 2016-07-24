1fadde34b2161ad0d6db0baba9879569ec82bf22
Ian Kennedy <exposure@gmail.com>
2016-07-24 18:21:33 -0400

    get-help complete. closes #5

--- wp-content/themes/nuci2016/page.php
+++ wp-content/themes/nuci2016/page.php
@@ -30,12 +30,60 @@ get_header(); ?>
       ?>
       <!-- let's loop through the top level pages and only add appropriate posts -->
       <?php if (is_page('get-help')) : ?>
-        <!-- get help page has most recent support post -->
-        <!-- TODO: insert posts here -->
+        <div class="post-cards-wrapper">
+          <div class="post-cards">
+          <?php
+            $args = array(
+              'posts_per_page' => 3,
+              'order' => 'DSC',
+              'orderby' => 'date',
+              'category' => '-193, 194'
+            );
+            $postslist = get_posts($args);
+            foreach ($postslist as $post):
+              setup_postdata($post);
+          ?>
+            <div class="post-card">
+              <div class="card-image">
+                <a href="<?php the_permalink();?>"><img src="<?php if (has_post_thumbnail()) {the_post_thumbnail_url();}?>" alt=""></a>
+              </div>
+              <div class="card-copy"><h4><a href="<?php the_permalink();?>"><?php the_title();?></a></h4>
+              <?php the_excerpt();?></div>
+            </div>
+          <?php
+            endforeach;
+            wp_reset_postdata();
+          ?>
+        </div><!-- .post-cards -->
+      </div><!-- .post-cards-wrapper -->
       <?php endif; ?>
       <?php if (is_page('help-us')) : ?>
-        <!-- help us page has most recent volunteer post -->
-        <!-- TODO: insert posts here -->
+        <div class="post-cards-wrapper">
+          <div class="post-cards">
+          <?php
+            $args = array(
+              'posts_per_page' => 3,
+              'order' => 'DSC',
+              'orderby' => 'date',
+              'category' => '-193, 192'
+            );
+            $postslist = get_posts($args);
+            foreach ($postslist as $post):
+              setup_postdata($post);
+          ?>
+            <div class="post-card">
+              <div class="card-image">
+                <a href="<?php the_permalink();?>"><img src="<?php if (has_post_thumbnail()) {the_post_thumbnail_url();}?>" alt=""></a>
+              </div>
+              <div class="card-copy"><h4><a href="<?php the_permalink();?>"><?php the_title();?></a></h4>
+              <?php the_excerpt();?></div>
+            </div>
+          <?php
+            endforeach;
+            wp_reset_postdata();
+          ?>
+        </div><!-- .post-cards -->
+      </div><!-- .post-cards-wrapper -->
       <?php endif; ?>
       <!-- about page has the most recent i am nucis post -->
       <?php if (is_page('about-us')) : ?>
